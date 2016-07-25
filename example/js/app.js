5a9fd15907363f688e35354b49e26ece4a6dd012
Rendol <sapegin@private-it.ru>
2016-07-25 15:21:00 +0700

    Вынос виджета из модуля

--- example/js/app.js
+++
@@ -1,9 +0,0 @@
-$(function () {
-    var container = '#' + $('#messenger-messages').find('[data-pjax-container]').attr('id');
-    $('#messenger-form').on('pjax:success', function () {
-        $.pjax.reload(container);
-    });
-    setInterval(function(){
-        $.pjax.reload(container);
-    }, 1000);
-});
